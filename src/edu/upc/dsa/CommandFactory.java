b734b27f92633559ee45611b58bdb57df130c040
krunal badsiwal <krunalmiracle@gmail.com>
2020-04-07 12:56:52 +0200

    Implemented Command Factory Not working null error!

---
+++ src/edu/upc/dsa/CommandFactory.java
@@ -0,0 +1,37 @@
+package edu.upc.dsa;
+import java.util.HashMap;
+public class CommandFactory {
+
+    //Patrón Singleton con Tabla de Hash
+    private HashMap<String, Command> MapNameToCommand;
+    private static CommandFactory instance;
+
+    private CommandFactory() {
+        this.MapNameToCommand = new HashMap<String, Command>();
+    }
+
+    public static CommandFactory getInstance() {
+        if (instance == null) instance = new CommandFactory();
+        return instance;
+    }
+
+    public Command getCommand(String name) {
+        Command command = this.MapNameToCommand.get(name);
+        if (command == null) {
+            command = getCommand2(name);
+            this.MapNameToCommand.put(name, command);
+        }
+        return command;
+    }
+    public static Command getCommand2(String name) {
+        Command command = null;
+        Class clazz = null;
+        try {
+            clazz = Class.forName(name);
+            command = (Command) clazz.newInstance();
+        } catch (IllegalAccessException | InstantiationException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return command;
+    }
+}
--- src/edu/upc/dsa/Main.java
+++ src/edu/upc/dsa/Main.java
@@ -3,6 +3,7 @@ package edu.upc.dsa;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Command command = CommandFactory.getInstance().getCommand("C1");
+        command.execute();
     }
 }
